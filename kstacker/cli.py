53909ffdc7c62f6a7bf114d81c6e435c7eb9b5a2
Simon Conseil <simon.conseil@lam.fr>
2022-03-14 17:40:38 +0100

    Add seed option to reproduce results

--- kstacker/cli.py
+++ kstacker/cli.py
@@ -1,12 +1,15 @@
 import argparse
 
+import numpy as np
 import yaml
 
 from .noise_profile import compute_noise_profiles
 
 # to plot in a file without a display screen (cluster)
+# isort: off
 import matplotlib
 matplotlib.use('Agg')  # noqa
+# isort: on
 
 
 def main():
@@ -16,6 +19,7 @@ def main():
     subparsers = parser.add_subparsers(title="subcommands", help="")
     sub1 = subparsers.add_parser("noise_profiles", help="compute noise profiles")
     sub1.add_argument("parameter_file", help="Parameter file (yml)")
+    sub1.add_argument("--seed", type=int, help="seed for random numbers")
     sub1.set_defaults(func=noise_profiles)
 
     args = parser.parse_args()
@@ -23,6 +27,8 @@ def main():
 
 
 def noise_profiles(args):
+    if args.seed:
+        np.random.seed(args.seed)
     with open(args.parameter_file) as f:
         params = yaml.safe_load(f)
 
